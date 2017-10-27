d792550f682c0d820eaafeb21faba3961884d10c
Luis Quintanilla <lquintanilla@lquintanilla-l.corp.instructure.com>
2017-10-27 07:27:43 -0600

    created "new" route

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ mongoose.connect("mongodb://localhost:27017/nodeJS_blog", {
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
 // MONGOOSE/MODEL CONFIG
 var blogSchema = new mongoose.Schema({
   title: String,
@@ -27,7 +28,7 @@ var Blog = mongoose.model("Blog", blogSchema);
 app.get("/", function(req, res){
     res.redirect("/blogs");
 });
-
+// INDEX ROUTE
 app.get("/blogs", function(req, res){
     Blog.find({}, function (err, blogs){
         if(err){
@@ -37,6 +38,11 @@ app.get("/blogs", function(req, res){
         }
     });
 });
+// NEW ROUTE
+app.get("/blogs/new", function(req, res){
+    res.render("new");
+});
+// CREATE ROUTE
 
 
 app.listen(3000, function() {
